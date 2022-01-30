741358279574d4ee2313b6e607dbafb6ea5bba2d
hollowblind095 <hollowblind095@gmail.com>
2022-01-30 11:40:22 -0300

    se creó componente Support

--- proyecto-react/src/App.js
+++ proyecto-react/src/App.js
@@ -1,10 +1,12 @@
 import NavBar from "./components/NavBar";
 import Footer from "./components/Footer";
+import Support from "./components/Support";
 
 function App() {
   return (
     <div className="flex flex-col">
       <NavBar/>
+      <Support/>
       <Footer/>
     </div>
   );
---
+++ proyecto-react/src/components/Support/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Support = () => {
+  return <div className="bg-green-500">Este es un componente Support</div>;
+};
+
+export default Support;
