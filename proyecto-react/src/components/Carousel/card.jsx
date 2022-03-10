c644044adbef206d14a6a584b4bd619dc7e92a4b
GermanParodi <germandanielparodi@gmail.com>
2022-03-10 16:29:43 -0300

    Correcciones varias, adaptaciones a responsive, modificación en Footer y conexión de varios componentes a Products, implementación de Modal a las cards mediante el boton

--- proyecto-react/src/components/Carousel/card.jsx
+++ proyecto-react/src/components/Carousel/card.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Card({ prop }) {
   const { url, alt } = prop;
@@ -8,17 +9,19 @@ function Card({ prop }) {
         flex flex-wrap
         sm:justify-evenly justify-center
         sm:bg-contain bg-no-repeat bg-colors-img sm:ease-in-out sm:delay-80 sm:hover:-translate-y-1 sm:hover:scale-110 sm:hover:bg-blend-hard-light sm:duration-300
-        sm:bg-slate-300 sm:p-8 sm:w-56 sm:h-60 w-full h-37
+        sm:bg-slate-300 p-4 sm:w-56 sm:h-60 w-full h-37
         sm:drop-shadow-md
         sm:rounded-md
         "
     >
       <div>
-        <img className="h-24 w-48" src={url} alt={alt} />
+        <img className="h-24 w-48 sm:mb-0 mb-2" src={url} alt={alt} />
       </div>
-      <p className="sm:text-center text-white sm:self-center self-end sm:ml-0 ml-20 sm:text-black sm:font-serif sm:mt-8 sm:text-base sm:hover:font-bold sm:hover:text-fuchsia-500">
-        See More
-      </p>
+      <Link to="/Products">
+        <p className="sm:text-center text-white sm:self-center self-end sm:text-black sm:font-serif sm:mt-8 sm:text-base sm:hover:font-bold sm:hover:text-fuchsia-500">
+          See More
+        </p>
+      </Link>
     </div>
   );
 }
